29bc011fc443168e753db2d4b3c7de4b5b50e834
Danick Loic Yagoue <dy@alphabytes.de>
2022-08-11 14:51:58 +0200

    set heroku production

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy, Model
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qdwcvoyytvfxey:bd846fbf3f369144517cd689a9556fd8c7c9f453947d4b1d44b7b9e66ec856e2@ec2-52-51-3-22.eu-west-1.compute.amazonaws.com:5432/ddet4nh1o1s9hs'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
